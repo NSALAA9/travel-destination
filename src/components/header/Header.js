4d2704a000baeb046798e76ff1eff88d73fa65d9
NSALAA9 <alaansairat99@gmail.com>
2023-05-09 08:28:11 +0000

    lab 12

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 import { Link } from "react-router-dom";
 
@@ -20,4 +20,58 @@ function Header() {
 }
 
 
+export default Header;*/
+
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header style={styles.header}>
+      <nav style={styles.nav}>
+        <ul style={styles.ul}>
+          <li style={styles.li}>
+            <Link to="/" style={styles.link}>
+              Home
+            </Link>
+          </li>
+          <li style={styles.li}>
+            <Link to="/tours" style={styles.link}>
+              Tours
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 export default Header;
+
+const styles = {
+  header: {
+    backgroundColor: "#333",
+    color: "#fff",
+    padding: "20px",
+    textAlign: "center",
+  },
+  nav: {
+    display: "flex",
+    justifyContent: "center",
+  },
+  ul: {
+    listStyle: "none",
+    margin: 0,
+    padding: 0,
+    display: "flex",
+  },
+  li: {
+    margin: "0 10px",
+  },
+  link: {
+    color: "#fff",
+    textDecoration: "none",
+    fontSize: "1.2rem",
+  },
+};
